99c324ef1ad90fa4f9a8a6727bb7718182a13d79
Tunisia Artope <artope@yahoo.com>
2022-04-05 16:13:26 -0400

    commit tree 30f2dcbff4a117e17ffd28a7e0b0ef9ff2bb75ea

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,30 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+    ]
+
+    def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
 end
+
+
+# Finally, add a custom validator to the Post model that ensures the title is sufficiently clickbait-y. The validator should add a validation error if the title does not contain:
+
+# "Won't Believe"
+# "Secret"
+# "Top [number]"
+# "Guess"
